65b10d44b797351addfd6efb2598338ca750a53d
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2021-11-27 00:01:21 +0200

    move it to working prototype

--- Program.cs
+++ Program.cs
@@ -5,10 +5,26 @@ namespace PasswordGenerator
 {
     class Program
     {
+        public static char[][] overallArray = 
+        {
+            new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' },
+
+            new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' },
+
+            new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'},
+
+            new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=',
+                         '+', '?', '<', '>', ':', ';', '.', ',', '|', '/', '~', '{', '}',
+                         '[', ']', '№', '\'', '\"','\\'}
+        };
+
         static void Main(string[] args)
         {
             string input = string.Empty;
             bool[] options = new bool[5];
+            Random random = new Random();
 
             int choice = default;
 
@@ -16,7 +32,7 @@ namespace PasswordGenerator
             int.TryParse(Console.ReadLine(), out int passwordLength);
 
 
-            while (input != "<")
+            while (input != ">")
             {
                 Console.Clear();
 
@@ -24,7 +40,8 @@ namespace PasswordGenerator
                                   "[2] Little letters\n" +
                                   "[3] Big letters\n" +
                                   "[4] Special Symbols\n" +
-                                  "[5] Avoid duplicates\n");
+                                  "[5] Avoid duplicates\n\n" +
+                                  "[>] Generate password\n");
 
                 Console.WriteLine("What do you want to use in the password?");
                 input = Console.ReadLine();
@@ -38,6 +55,24 @@ namespace PasswordGenerator
 
                 if (counterOfOptions == 5) break;
             }
+
+            List<char> test = new List<char>();
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (options[i])
+                {
+                    for (int j = 0; j < overallArray[i].Length; j++)
+                    {
+                        test.Add(overallArray[i][j]);
+                    }
+                }
+            }
+
+            char[] password = new char[passwordLength];
+            for (int i = 0; i < password.Length; i++)
+                password[i] = test[random.Next(0, test.Count)];
+
+            Console.WriteLine($"\n\nYour password is: {new string(password)}");
         }
     }
 }
